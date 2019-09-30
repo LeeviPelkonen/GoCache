25d996b882441f2c608c072bec2c4c02e54d65bf
Lassi Ylirautalahti <32744095+Buetix@users.noreply.github.com>
2019-09-30 14:48:36 +0300

    Added google OAuth credentials

---
+++ app/src/main/java/com/example/gocache/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gocache
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    // Client ID for google login(OAuth client): 71030826346-0h97ljq07nsktjct9lb4egbomda96o1r.apps.googleusercontent.com
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
