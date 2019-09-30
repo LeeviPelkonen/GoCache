60aac986bc17d20ac962251877d940214d7b0cfb
Lassi Ylirautalahti <32744095+Buetix@users.noreply.github.com>
2019-09-30 21:12:34 +0300

    Added google login, next step is to move after successfull login

--- app/src/main/java/com/example/gocache/LoginActivity.kt
+++ app/src/main/java/com/example/gocache/LoginActivity.kt
@@ -1,7 +1,17 @@
 package com.example.gocache
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_login.view.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -11,5 +21,46 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        fun signIn() {
+            val signInIntent: Intent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 100)
+        }
+
+        val signInButton = findViewById<com.google.android.gms.common.SignInButton>(R.id.sign_in_button)
+        signInButton.setOnClickListener {
+            when(it.id) {
+                R.id.sign_in_button -> signIn()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+            try {
+                val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)!!
+            } catch (e: ApiException) {
+                Log.w("lofinFail", "signInresult: failed code=" + e.statusCode)
+            }
+        }
+
+        if (requestCode == 100) run {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+
     }
 }
