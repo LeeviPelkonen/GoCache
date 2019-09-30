dd29fb6c82fb9d1815059b57434d656d6cece3e5
LeeviPelkonen <32296789+LeeviPelkonen@users.noreply.github.com>
2019-09-30 20:53:04 +0300

    my current location

--- app/src/main/java/com/example/gocache/MainActivity.kt
+++ app/src/main/java/com/example/gocache/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.gocache
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -25,5 +29,18 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        if ((Build.VERSION.SDK_INT >= 23 &&
+                    ContextCompat.checkSelfPermission(
+                        this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
+                    ) !=
+                    PackageManager.PERMISSION_GRANTED)
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                0
+            )
+        }
     }
 }
--- app/src/main/java/com/example/gocache/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/gocache/ui/dashboard/DashboardFragment.kt
@@ -1,15 +1,29 @@
 package com.example.gocache.ui.dashboard
 
 
+import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.gocache.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+
 
 
 
@@ -31,6 +45,35 @@ class DashboardFragment : Fragment(), OnMapReadyCallback {
         mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(this)
 
+        var locationManager =
+            activity!!.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        var locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location?) {
+                var latitute = location!!.latitude
+                var longitute = location!!.longitude
+
+                Log.i("test", "Latitute: $latitute ; Longitute: $longitute")
+
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+            }
+
+        }
+
+        try {
+            locationManager!!.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+        } catch (ex:SecurityException) {
+            Log.d("QWERTY",ex.toString())
+        }
+
         return rootView
     }
 
@@ -41,5 +84,4 @@ class DashboardFragment : Fragment(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(campus).title("Marker in Myyrmäki campus"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(campus))
     }
-
 }
